cf917c013f8d214ce0aae4e9bbbad26596ec1f1c
JESERYL MAE COMIA <128215077+JeserylMae@users.noreply.github.com>
2024-03-03 09:23:06 +0800

    Create homeGui.py

---
+++ src/homeGui.py
@@ -0,0 +1,25 @@
+
+import gui
+
+class HomeGui(GUI):
+
+    def display_home(self):
+    """
+    contains the main window for home page
+    Ica-call lang din mga GUI functions dito
+    para mabuo yong home page. The yong choice
+    ng user about kung anong gusto nyang gamitin 
+    (video or camera ba) ay ipi-print na lang muna
+    console.
+    """
+    def create_buttons(self, parent):
+    """
+    Set the default elements ng button.
+    :return: button
+    """
+    def create_drop_menu(self, parent):
+        """
+        Set the default elements of drop
+        down menu
+        :return: drop_menu
+        """
