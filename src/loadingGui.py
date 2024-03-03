b153f126abbc3720a3a7e2bec5d07615705e4c4d
JESERYL MAE COMIA <128215077+JeserylMae@users.noreply.github.com>
2024-03-03 09:24:01 +0800

    Create loadingGui.py

---
+++ src/loadingGui.py
@@ -0,0 +1,11 @@
+import src.gui
+
+class LoadingGui(GUI):
+    def display_loading_animation(self):
+    """
+    contains the main window for loading page.
+    
+    Dito ica-call yong mga functions from GUI, 
+    para mabuo yong designs ng loading page.
+    (Ang template ay nasa figma).
+    """
