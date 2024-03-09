9fc0797efb6a0c001479020d196e06fd1634a187
JESERYL MAE COMIA <128215077+JeserylMae@users.noreply.github.com>
2024-03-09 20:24:48 +0800

    Add files via upload

---
+++ src/detect.py
@@ -0,0 +1,18 @@
+import cv2
+
+
+class Detect:
+
+    @staticmethod
+    def count_cameras():
+        cam_num = list()
+        for i in range(10):
+            cap = cv2.VideoCapture(i)
+            if cap.isOpened():
+                cam_num.append(f"Camera {i}")
+                print(f"Camera {i} is available...")
+            else:
+                break
+
+            cap.release()
+        return cam_num
