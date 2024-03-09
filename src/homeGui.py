65bbc1dfa0f2ef92768268ec65fcf9ebe75b8936
JESERYL MAE COMIA <128215077+JeserylMae@users.noreply.github.com>
2024-03-09 20:26:39 +0800

    Update homeGui.py

--- src/homeGui.py
+++ src/homeGui.py
@@ -1,25 +1,100 @@
+import tkinter as tk
+from tkinter import filedialog
+from src.gui import GUI
+from src.detect import Detect
+from threading import Thread
+from time import sleep
 
-import src.gui
 
 class HomeGui(GUI):
+    def __init__(self, window, screen_width, screen_height, video_img, cam_img):
+        self.__window = window
+        self.__window_height = screen_height
+        self.__window_width = screen_width
+        self.__black = self.get_black()
+        self.__white = self.get_white()
+        self.__gray = self.get_gray()
+        self.__dark_gray = self.get_darker_gray()
+        self.__video_img = video_img
+        self.__cam_img = cam_img
+        self.__camera_list = list()
+        self.__video_path = " "
 
-    def display_home(self):
-    """
-    contains the main window for home page
-    Ica-call lang din mga GUI functions dito
-    para mabuo yong home page. The yong choice
-    ng user about kung anong gusto nyang gamitin 
-    (video or camera ba) ay ipi-print na lang muna
-    console.
-    """
-    def create_buttons(self, parent):
-    """
-    Set the default elements ng button.
-    :return: button
-    """
-    def create_drop_menu(self, parent):
-        """
-        Set the default elements of drop
-        down menu
-        :return: drop_menu
-        """
+    def display_home(self, cosmic_icon):
+        frame = self.create_frame(parent=self.__window, height=self.__window_height - 90,
+                                  width=self.__window_width - 60, pos_x=(self.__window_width / 64),
+                                  pos_y=(self.__window_width / 64))
+        Thread(target=self.__get_camera_list).start()
+
+        self.__display_icon_and_title(frame, cosmic_icon)
+        sleep(0.002)
+        self.__create_rectangle(frame)
+        sleep(0.002)
+
+        # Create buttons.
+        open_video_btn = self.__create_buttons(parent=frame, img=self.__video_img, color=self.__dark_gray,
+                                               label="Open video", X=1.2, Y=3, bd=0,
+                                               cmd=self.__create_file_dialog)
+        use_camera_btn = self.__create_buttons(parent=frame, img=self.__cam_img, color=self.__black,
+                                               label="Use camera", X=0.85, Y=3, bd=3,
+                                               cmd=lambda: self.__create_drop_menu(frame))
+
+    def __display_icon_and_title(self, frame, cosmic_icon):
+        img_frame = self.create_frame(parent=frame, height=(self.__window_height / 3),
+                                      width=(self.__window_width / 2.8), pos_x=(self.__window_width / 80),
+                                      pos_y=(self.__window_height / 3))
+
+        img_label = self.create_img_label(parent=img_frame, img=cosmic_icon)
+        img_label.place(x=200, y=0)
+
+        # Add page title.
+        text_position = 200
+        self.create_text_title(parent=img_frame, text="S", pos_x=230, pos_y=text_position)
+        self.create_text_title(parent=img_frame, text="co", pos_x=80, pos_y=text_position)
+        self.create_text_title(parent=img_frame, text="mic", pos_x=350, pos_y=text_position)
+
+    def __create_rectangle(self, parent):
+        self.create_frame(parent=parent, height=(self.__window_height - 200), width=(self.__window_width / 1.9),
+                          pos_y=(self.__window_height / 16), pos_x=(self.__window_width / 2.6)
+                          ).config(bg=self.__black, bd=2, relief='groove', highlightcolor=self.__white)
+
+    def __create_buttons(self, parent, img, color, label, X, Y, bd, cmd):
+        def on_enter(e):
+            button['background'] = self.__gray
+
+        def on_leave(e):
+            button['background'] = color
+
+        button = tk.Button(master=parent, bg=color, font=("Roboto Mono", 18), fg=self.__white,
+                           height=self.__window_height / 3.5, width=self.__window_height / 3.5,
+                           bd=bd, relief='groove', image=img, text=label, compound="top",
+                           highlightcolor=self.__white, activebackground=color,
+                           command=cmd)
+
+        button.bind("<Enter>", on_enter)
+        button.bind("<Leave>", on_leave)
+        button.place(x=int(self.__window_height / X), y=int(self.__window_height / Y))
+
+        return button
+
+    def __get_camera_list(self):
+        self.__camera_list = Detect.count_cameras()
+
+    def __create_drop_menu(self, frame):
+        clicked = tk.StringVar()
+        clicked.set(self.__camera_list[0])
+
+        drop_menu = tk.OptionMenu(frame, clicked, *self.__camera_list)
+        drop_menu.place(x=int(self.__window_width/1.94), y=(self.__window_height/1.45))
+        drop_menu.config(font=("Roboto Mono", 15), width=40, height=2, justify='center',
+                         relief='flat', bg=self.__black, highlightthickness=2,
+                         highlightbackground=self.__gray, fg=self.__white)
+        print(f"Use {clicked.get()}\n")
+
+    def __create_file_dialog(self):
+        video_file = filedialog.askopenfilename(
+            initialdir="/", title="Select file",
+            filetypes=(("video files", "*.mp4;*.avi;*.mkv;*.wav"), ("all files", "*.*"))
+        )
+        self.__video_path = video_file
+        print(f"Video Path: {self.__video_path}\n")
