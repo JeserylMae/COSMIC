87cc9f19590fed5e130f2b454952fc3dcdb5c884
JESERYL MAE COMIA <128215077+JeserylMae@users.noreply.github.com>
2024-03-26 22:08:38 +0800

    Update loadingGui.py

--- src/loadingGui.py
+++ src/loadingGui.py
@@ -1,26 +1,81 @@
+import tkinter as tk
 from src.gui import GUI
-
-
-class ImageWindow:
-    def __init__(self, window, img_path, width, height):
-        self.window = window
-        self.img_path = 'src/images/image-removebg-preview.png'
-        self.image = tk.PhotoImage(file=self.img_path)
-        self.resized_img = self.image.subsample(width, height)
-
-        img_label = tk.Label(window, image=self.resized_img, bg="black")
-        img_label.place(x=0, y=0)
+from time import sleep
 
 
 class LoadingGui(GUI):
+    def __init__(self, window, window_height, window_width, dot_img1, dot_img2, barcode_img, logo_img):
+        super().__init__()
+        self.__window = window
+        self.__window_height = window_height
+        self.__window_width = window_width
+        self.__dot_img1 = dot_img1
+        self.__dot_img2 = dot_img2
+        self.__barcode_img = barcode_img
+        self.__logo_img = logo_img
+
     def display_loading_animation(self):
-        window_width, window_height = self.set_window_attributes(self.window)
+        frame = self.create_frame(parent=self.__window, height=self.__window_height,
+                                  width=self.__window_width, pos_x=0,
+                                  pos_y=0)
 
-        loading_img_path = 'src/images/image-removebg-preview.png'
-        loading_img = ImageWindow(self.window, loading_img_path, 16, 16)
+        barcode_label = self.create_img_label(parent=frame, img=self.__barcode_img)
+        barcode_label.place(x=self.__window_width / 2.25, y=self.__window_height / 8)
 
-        loading_text = "LOADING COSMIC\nCORRIDOR OCCUPANCY SOUND MONITORING AND IDENTIFICATION SYSTEM"
-        self.create_text_title(self.window, loading_text, window_width // 2 - 350, window_height // 2 - 200)
+        self.__display_logo_title(frame)
 
-        version_text = "Version 1.0.0"
-        self.create_text_body(self.window, version_text)
+        cosmic_meaning = "Corridor Occupancy Sound Monitoring And Identification System"
+        self.create_text_body(parent=frame, text=cosmic_meaning, pos_x=(self.__window_width / 3.25),
+                              pos_y=(self.__window_height / 2))
+
+        self.__display_loading_animation(parent=frame)
+
+        frame.destroy()
+
+    def __display_logo_title(self, frame):
+        title_logo_frame = self.create_frame(parent=frame, height=(self.__window_height / 4),
+                                             width=(self.__window_width / 1.4), pos_x=(self.__window_width / 8),
+                                             pos_y=(self.__window_height / 5))
+        title_logo_frame.config(bd=(self.__window_width / 48), highlightcolor=self._gray, relief=tk.SUNKEN)
+
+        # Add application logo.
+        logo_img_label = self.create_img_label(parent=title_logo_frame, img=self.__logo_img)
+        logo_img_label.place(x=(self.__window_width / 6.15), y=(self.__window_height / 40))
+
+        # Add page title.
+        text_position = (self.__window_height / 4) / 7
+        x_text_position = (self.__window_width / 4.05)
+        self.create_text_title(parent=title_logo_frame, text="S", pos_x=(x_text_position + 150), pos_y=text_position)
+        self.create_text_title(parent=title_logo_frame, text="co", pos_x=x_text_position, pos_y=text_position)
+        self.create_text_title(parent=title_logo_frame, text="mic", pos_x=(x_text_position + 270), pos_y=text_position)
+
+    def __display_loading_animation(self, parent):
+        self.__dot_img_label1 = self.create_img_label(parent=parent, img=self.__dot_img1)
+        self.__dot_img_label2 = self.create_img_label(parent=parent, img=self.__dot_img2)
+        self.__dot_img_label3 = self.create_img_label(parent=parent, img=self.__dot_img2)
+
+        self.__x_position = self.__window_width / 2.1
+        self.__y_position = self.__window_height / 1.25
+
+        for i in range(4):
+            self.__animate(parent)
+
+    def __animate(self, parent):
+        for i in range(3):
+            if i == 0:
+                self.__dot_img_label1.place(x=self.__x_position - 40, y=self.__y_position)
+                self.__dot_img_label2.place(x=self.__x_position, y=self.__y_position)
+                self.__dot_img_label3.place(x=self.__x_position + 40, y=self.__y_position)
+
+            elif i == 1:
+                self.__dot_img_label2.place(x=self.__x_position - 40, y=self.__y_position)
+                self.__dot_img_label1.place(x=self.__x_position, y=self.__y_position)
+                self.__dot_img_label3.place(x=self.__x_position + 40, y=self.__y_position)
+
+            elif i == 2:
+                self.__dot_img_label2.place(x=self.__x_position - 40, y=self.__y_position)
+                self.__dot_img_label3.place(x=self.__x_position, y=self.__y_position)
+                self.__dot_img_label1.place(x=self.__x_position + 40, y=self.__y_position)
+
+            parent.update_idletasks()
+            sleep(0.5)
