d3ff6598c3d25c5e86316cc8d8a9b1b971b15f25
CHUSTINE MAE CANTAL <129162395+chustinecantal@users.noreply.github.com>
2024-04-19 23:39:22 +0800

    Update sound.py

--- src/sound.py
+++ src/sound.py
@@ -4,13 +4,19 @@ import librosa
 
 
 class SoundDetector:
-    def __init__(self):
-        self.threshold = 0.1
-        self.chunk_size = 1024
-        self.format = pyaudio.paInt16
-        self.channels = 1
-        self.rate = 44100
-        self.max_record_time = 30
+    def __init__(self, threshold=0.1, chunk_size=1024, format=pyaudio.paInt16, channels=1, rate=44100, max_record_time=30):
+        #self.threshold = 0.1
+        self.threshold = threshold
+        self.chunk_size = chunk_size
+        #self.chunk_size = 1024
+        self.format = format
+        #self.format = pyaudio.paInt16
+        self.channels = channels
+        #self.channels = 1
+        #self.rate = 44100
+        self.rate = rate
+        self.max_record_time = max_record_time
+        #self.max_record_time = 30
         self.audio = pyaudio.PyAudio()
         self.stream = self.audio.open(format=self.format,
                                       channels=self.channels,
@@ -20,10 +26,15 @@ class SoundDetector:
 
     def detect_sound(self):
         try:
+            print("Recording sound for 30 seconds...")
             recorded_frames = self.record_sound()
+            print("Recording finished. Number of recorded frames: ", len(recorded_frames))
             sound_detected = False
             for data in recorded_frames:
-                if np.max(data) > self.threshold * np.iinfo(np.int16).max:
+                audio_data = np.frombuffer(data, dtype=np.int16)
+                normalized_audio = audio_data / np.iinfo(np.int16).max
+                if np.max(normalized_audio) > self.threshold:
+               # if np.max(data) > self.threshold * np.iinfo(np.int16).max:
                     print("Sound detected!")
                     sound_detected = True
                     break
@@ -49,6 +60,11 @@ class SoundDetector:
             pass
         return frames
 
+    if __name__ == "__main__":
+        #adjust threshold mas mababa ay mas sensitive daw
+        threshold_level = 0.05
+        detector = SoundDetector(threshold=threshold_level)
+        detector.detect_sound()
     # def calculate_intensity(self, data):
     #    rms_amplitude = librosa.feature.rms(y=np.frombuffer(data, dtype=np.int16))[0]
     #     intensity_db = librosa.amplitude_to_db(rms_amplitude, ref=np.max)[0]
