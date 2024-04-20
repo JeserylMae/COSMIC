cedb13423ed23bc0196e93105a213cfbb2ae1fcc
JESERYL MAE COMIA <128215077+JeserylMae@users.noreply.github.com>
2024-04-20 12:27:38 +0800

    Update sound.py

--- src/sound.py
+++ src/sound.py
@@ -1,77 +1,26 @@
-import pyaudio
-import numpy as np
-import librosa
+import sounddevice as sd
+import wave
 
 
-class SoundDetector:
-    def __init__(self, threshold=0.1, chunk_size=1024, format=pyaudio.paInt16, channels=1, rate=44100, max_record_time=30):
-        #self.threshold = 0.1
-        self.threshold = threshold
-        self.chunk_size = chunk_size
-        #self.chunk_size = 1024
-        self.format = format
-        #self.format = pyaudio.paInt16
-        self.channels = channels
-        #self.channels = 1
-        #self.rate = 44100
-        self.rate = rate
-        self.max_record_time = max_record_time
-        #self.max_record_time = 30
-        self.audio = pyaudio.PyAudio()
-        self.stream = self.audio.open(format=self.format,
-                                      channels=self.channels,
-                                      rate=self.rate,
-                                      input=True,
-                                      frames_per_buffer=self.chunk_size)
+class Sound:
+    def __init__(self):
+        self.__duration = 30
+        self.__fs = 44100
+        self.__recording = None
 
-    def detect_sound(self):
-        try:
-            print("Recording sound for 30 seconds...")
-            recorded_frames = self.record_sound()
-            print("Recording finished. Number of recorded frames: ", len(recorded_frames))
-            sound_detected = False
-            for data in recorded_frames:
-                audio_data = np.frombuffer(data, dtype=np.int16)
-                normalized_audio = audio_data / np.iinfo(np.int16).max
-                if np.max(normalized_audio) > self.threshold:
-               # if np.max(data) > self.threshold * np.iinfo(np.int16).max:
-                    print("Sound detected!")
-                    sound_detected = True
-                    break
-                else:
-                    print("Silence")
-            if not sound_detected:
-                print("No sound detected during recording.")
-        except KeyboardInterrupt:
-            pass
-        finally:
-            self.stream.stop_stream()
-            self.stream.close()
-            self.audio.terminate()
+    def sound_detect(self):
+        print("Recording..")
+        self.__recording = sd.rec(int(self.__duration * self.__fs), samplerate=self.__fs,
+                                  channels=1, dtype='int16')
+        sd.wait()  # Wait for the recording to finish
+        print("Recording finished.")
 
-    def record_sound(self):
-        frames = []
-        max_record_frames = int(self.rate / self.chunk_size * self.max_record_time)
-        try:
-            for _ in range(max_record_frames):
-                data = self.stream.read(self.chunk_size)
-                frames.append(data)
-        except KeyboardInterrupt:
-            pass
-        return frames
+    def save_audio_as_wav(self):
+        filename = "recoding.wav"
 
-    if __name__ == "__main__":
-        #adjust threshold mas mababa ay mas sensitive daw
-        threshold_level = 0.05
-        detector = SoundDetector(threshold=threshold_level)
-        detector.detect_sound()
-    # def calculate_intensity(self, data):
-    #    rms_amplitude = librosa.feature.rms(y=np.frombuffer(data, dtype=np.int16))[0]
-    #     intensity_db = librosa.amplitude_to_db(rms_amplitude, ref=np.max)[0]
-    #     return intensity_db
+        with wave.open(filename, 'wb') as wf:
+            wf.setnchannels(1)
+            wf.setsampwidth(2)  # 16-bit PCM
+            wf.setframerate(self.__fs)
+            wf.writeframes(self.__recording.tobytes())
 
-
-#
-# if _name_ == "_main_":
-#     detector = SoundDetector()
-#     detector.detect_sound()
